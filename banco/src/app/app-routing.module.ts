9d517bf6a7b912c680f5092a4902955a6e125b25
Jonathan Bustos <jonathanbust@gmail.com>
2022-06-18 04:54:52 -0400

    first commit

---
+++ banco/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './vistas/login/login.component';
+import { NuevoComponent } from './vistas/nuevo/nuevo.component';
+import { EditarComponent } from './vistas/editar/editar.component';
+import { DashboardComponent } from './vistas/dashboard/dashboard.component';
+const routes: Routes = [
+  { path:'', redirectTo:'login', pathMatch:'full'},
+  { path:'login', component:LoginComponent },
+  { path:'nuevo', component:NuevoComponent },
+  { path:'editar', component:EditarComponent },
+  { path:'dashboard', component:DashboardComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
